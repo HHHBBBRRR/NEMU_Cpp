45dfabd6c21c109b72b8023008fa462a57cd7a45
HHHBBBRRR <3543977024@qq.com>
2025-02-08 17:57:19 +0800

    ✨ feat:  implement CPU class and instruction handling
    - Add CPU class with basic functionality for registers and program counter
    - Create InstructionFactory class for instruction creation and mapping
    - Implement instruction fetching, decoding, and execution workflow
    - Add support for RVI and RVM instruction sets

---
+++ include/cpu.h
@@ -0,0 +1,41 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <array>
+#include <bitset>
+#include <cstdint>
+#include <memory>
+#include <unordered_map>
+#include "common.h"
+
+class Instruction;
+
+class InstructionFactory
+{
+public:
+	InstructionFactory();
+	std::shared_ptr<Instruction> createInstruction(word_t inst);
+
+private:
+	std::unordered_map<std::bitset<7>, std::shared_ptr<Instruction>> m_instructionMap;
+};
+
+class CPU
+{
+public:
+	word_t getGPR(std::uint32_t idx) const;
+	void setGPR(std::uint32_t idx, word_t value);
+	word_t getPC() const;
+	void setPC(word_t value);
+	void isa_exec_once();
+
+private:
+	inline word_t inst_fetch() const;
+
+	std::array<word_t, NUM_REGS> m_GPR{};		// General Purpose Register
+	word_t m_PC{ MBASE };		// Program Counter
+
+	InstructionFactory m_factory;
+};
+
+#endif // CPU_H
