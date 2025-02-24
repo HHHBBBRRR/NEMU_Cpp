270b98bdd7380d48eeb7945cfe12971bc5b6c3e5
HHHBBBRRR <3543977024@qq.com>
2025-02-24 21:29:41 +0800

    ✨ feat: add state management for NEMU
    - Introduce NEMUState struct to manage simulation state
    - Implement state transition logic
    - Add trap and invalid instruction handling

---
+++ include/state.h
@@ -0,0 +1,33 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <iostream>
+#include "common.h"
+
+enum class STATE
+{
+	NEMU_RUNNING,
+	NEMU_STOP,
+	NEMU_END,   // guest exit successfully
+	NEMU_ABORT, // guest exit failure
+};
+
+struct NEMUState
+{
+    STATE m_state;
+    paddr_t m_halt_pc;
+    std::uint32_t m_halt_ret;
+
+    NEMUState() 
+        : m_state{STATE::NEMU_RUNNING}, m_halt_pc{0x8000'0000}, m_halt_ret{1}
+    {}
+    void set_nemu_state(STATE state, paddr_t halt_pc, std::uint32_t halt_ret);
+};
+
+extern NEMUState nemu_state;
+
+void nemu_trap(paddr_t thispc, std::uint32_t code);
+
+void invalid_inst(paddr_t thispc);
+
+#endif // !STATE_H
---
+++ src/state.cpp
@@ -0,0 +1,21 @@
+#include "state.h"
+
+NEMUState nemu_state;
+
+void NEMUState::set_nemu_state(STATE state, paddr_t halt_pc, std::uint32_t halt_ret)
+{
+    m_state = state;
+    m_halt_pc = halt_pc;
+    m_halt_ret = halt_ret;
+}
+
+void nemu_trap(paddr_t thispc, std::uint32_t code)
+{
+    nemu_state.set_nemu_state(STATE::NEMU_END, thispc, code);
+}
+
+void invalid_inst(paddr_t thispc)
+{
+    std::cout << "Invalid instruction at pc = " << thispc << std::endl;
+    nemu_state.set_nemu_state(STATE::NEMU_ABORT, thispc, 1);
+}
